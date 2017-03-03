16666ca6a1e8997b4505d2905f165fee5688f455
Saini Kumara <saini@iki.fi>
2017-03-03 17:59:23 +0200

    korjasin Keskustelunavaus.javan

--- src/main/java/tikape/runko/domain/Keskustelunavaus.java
+++ src/main/java/tikape/runko/domain/Keskustelunavaus.java
@@ -7,8 +7,52 @@ package tikape.runko.domain;
 
 /**
  *
- * @author eetu
+ * @author lvikstro
  */
-class Keskustelunavaus {
+public class Keskustelunavaus {
+    private int id;
+    private int alue;
+    private int aika; 
+    private String otsikko;
+
+    public Keskustelunavaus(int id, int alue, int aika, String otsikko) {
+        this.id= id;
+        this.alue= alue;
+        this.aika= aika;
+        this.otsikko=otsikko;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAlue(int alue) {
+        this.alue = alue;
+    }
+
+    public void setAika(int aika) {
+        this.aika = aika;
+    }
+
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAlue() {
+        return alue;
+    }
+
+    public int getAika() {
+        return aika;
+    }
+
+    public String getOtsikko() {
+        return otsikko;
+    }
     
 }
